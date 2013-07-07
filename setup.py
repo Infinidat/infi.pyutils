b505ce65999c7333b46d4c99389db56856d5cdc7
Rotem Yaari <rotemy@infinidat.com>
2013-07-07 20:31:34 +0300

    Reenable Python 3.3
    * Rely on emport for import logic

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import itertools
 import platform
 from setuptools import setup, find_packages
@@ -6,23 +7,25 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.dirname(__file__), "infi", "pyutils", "__version__.py")) as version_file:
     exec(version_file.read())
 
-install_requires = ["mock", "six"]
-if platform.python_version() < '2.7':
+install_requires = ["mock", "six", "emport"]
+if sys.version_info < (2, 7):
     install_requires.append('unittest2')
     install_requires.append('ordereddict')
 
 setup(name="infi.pyutils",
       classifiers = [
           "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
           ],
       description="Misc. pure-python utilities",
       license="BSD",
       author="Rotem Yaari",
       author_email="vmalloc@gmail.com",
-      #url="your.url.here",
       version=__version__,
       packages=find_packages(exclude=["tests"]),
       namespace_packages=["infi"],
       install_requires=install_requires,
+      zip_safe=False,
       scripts=[],
       )
